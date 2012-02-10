b96e3d141505247a4d7e6251fc1da5236d69796e
Albert Cho <albert.hyun.cho@gmail.com>
2012-02-09 16:22:37 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drugs::Application.config.secret_token = 'ed1fac95bb060cef48c95c439be439d23415eb1169decffdfd37f664581a79b5c623d3126c60e7a04775924683172fabb794462fada678925925aa7a79a3628d'
