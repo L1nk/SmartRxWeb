3774097eb38c4f6d84d5f4efe39befe139b00450
Albert Cho <albert.hyun.cho@gmail.com>
2012-02-25 15:54:06 -0800

    removed a migration

--- db/migrate/20120225222511_remove_user_id_from_schedule_to_entries.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromScheduleToEntries < ActiveRecord::Migration
-  def up
-    remove_column :entries, :user_id
-      end
-
-  def down
-    add_column :entries, :user_id, :integer
-  end
-end
